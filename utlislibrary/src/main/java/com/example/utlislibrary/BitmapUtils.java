f63e479a71655b7341d2caa16e92ca85a01a778a
DESKTOP-NO9V8JL\dell <693962802@qq.com>
2019-12-11 15:16:14 +0800

    通过 Drawable 图片资源获取 Bitmap

--- utlislibrary/src/main/java/com/example/utlislibrary/BitmapUtils.java
+++ utlislibrary/src/main/java/com/example/utlislibrary/BitmapUtils.java
@@ -1,6 +1,8 @@
 package com.example.utlislibrary;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -8,6 +10,12 @@ import android.graphics.drawable.Drawable;
 public class BitmapUtils {
 
 
+    /**
+     * 根据 drawable 的规则生成一个Bitmap
+     *
+     * @param drawable
+     * @return
+     */
     public static Bitmap drawableToBitmap(Drawable drawable) {
         Bitmap bitmap = null;
 
@@ -29,5 +37,24 @@ public class BitmapUtils {
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
         drawable.draw(canvas);
         return bitmap;
+    }
+
+
+    /**
+     * 通过 Drawable 图片资源获取 Bitmap
+     *
+     * @param res     Resources
+     * @param resId   图片资源Id
+     * @param density 表示 Bitmap 最终的像素密度
+     * @return
+     */
+    public static Bitmap getBitmapByDrawableResources(Resources res, int resId, int density) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        options.inJustDecodeBounds = false;
+        options.inDensity = options.outWidth;
+        options.inTargetDensity = density;
+        return BitmapFactory.decodeResource(res, resId, options);
     }
 }
