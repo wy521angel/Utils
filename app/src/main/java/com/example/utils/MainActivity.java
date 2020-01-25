5e9a9a65cfdb98dfd11d8acc41cc7749798c4d27
wy521angel <693962802@qq.com>
2020-01-25 22:34:54 +0800

    获取assets下的文本内容

--- app/src/main/java/com/example/utils/MainActivity.java
+++ app/src/main/java/com/example/utils/MainActivity.java
@@ -2,20 +2,35 @@ package com.example.utils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.utlislibrary.FileUtils;
 import com.example.utlislibrary.ScreenUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        c = this;
 
         //屏幕宽高
         int screenWidth = ScreenUtils.getScreenMetrics(this).widthPixels;
         int screenHeight = ScreenUtils.getScreenMetrics(this).heightPixels;
-        
+
+        TextView tv1 = findViewById(R.id.tv1);
+        tv1.setText("屏幕的宽为：" + screenWidth + "；屏幕的高为：" + screenHeight);
+
+        TextView tv2 = findViewById(R.id.tv2);
+        tv2.setText(FileUtils.getTextFromAssets(c, "Text_Test.txt"));
+
+        String writeFileString = "写入文件的测试";
+        FileUtils.writeFile(writeFileString, "Write_File.txt");
+
     }
 }
--- utlislibrary/src/main/java/com/example/utlislibrary/FileUtils.java
+++ utlislibrary/src/main/java/com/example/utlislibrary/FileUtils.java
@@ -1,4 +1,78 @@
 package com.example.utlislibrary;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+
+//注释掉的代码表示还没有测试过
 public class FileUtils {
+
+
+    /**
+     * 获取assets下的文本内容
+     *
+     * @param c
+     * @param fileName
+     * @return
+     */
+    public static String getTextFromAssets(Context c, String fileName) {
+        try {
+            InputStreamReader inputReader = new InputStreamReader(c.getResources().getAssets().open(fileName));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            StringBuilder result = new StringBuilder();
+            while ((line = bufReader.readLine()) != null)
+                result.append(line + "\n");
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+//    public static void writeFile(String text, String fileName) {
+//        try (OutputStream outputStream = new FileOutputStream(fileName);
+//             Writer writer = new OutputStreamWriter(outputStream);
+//             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+//            bufferedWriter.write(text);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    /**
+//     * 文件复制
+//     *
+//     * @param oldFileName
+//     * @param newFileName
+//     */
+//    public static void copyFile(String oldFileName, String newFileName) {
+//        try (InputStream inputStream = new FileInputStream(oldFileName);
+//             OutputStream outputStream = new FileOutputStream(newFileName)) {
+//            byte[] bytes = new byte[1024];
+//            int read;
+//            while ((read = inputStream.read(bytes)) != -1) {
+//                outputStream.write(bytes, 0, read);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
